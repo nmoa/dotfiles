d54a3ff0992cd391c7c46bf0c7e1fc32fd62b40e
Jumpei Yamaguchi <28538293+nmoa@users.noreply.github.com>
2023-03-13 22:26:06 +0900

    add .config .git

---
+++ dot_config/fish/functions/executable_compile.fish
@@ -0,0 +1,28 @@
+#!/usr/bin/fish
+
+function compile
+    argparse 's/sync' 'c/clean' 'm/dry-run-make' 'n/dry-run-sync' -- $argv
+    or return
+    if set -lq _flag_s
+        set is_delete '--delete'
+    end
+    if [ $argv[1] = "." ];
+        set dirname "."
+        set dirname_sjis (basename "$PWD" | iconv -f utf8 -t sjis)
+    else
+        set dirname (basename $argv[1])/
+        set dirname_sjis (basename "$dirname" | iconv -f utf8 -t sjis)
+    end
+    echo "$dirname"
+    echo "$dirname_sjis"
+    echo $is_delete
+    if set -lq _flag_n
+        rsync -avn $is_delete --exclude=".git" --exclude="build/*" "$dirname" "cd997:~/code/$dirname_sjis"
+    else if set -lq _flag_m
+        rsync -av $is_delete --exclude=".git" --exclude="build/*" "$dirname" "cd997:~/code/$dirname_sjis" && cocot -- ssh cd997 "cd code/$dirname_sjis/src; make -n $argv[2]"
+    else if set -lq _flag_c
+        rsync -av $is_delete --exclude=".git" --exclude="build/*" "$dirname" "cd997:~/code/$dirname_sjis" && cocot -- ssh cd997 "cd code/$dirname_sjis/src; make clean && lsfmake $argv[2]"
+    else
+        rsync -av $is_delete --exclude=".git" --exclude="build/*" "$dirname" "cd997:~/code/$dirname_sjis" && cocot -- ssh cd997 "cd code/$dirname_sjis/src; lsfmake $argv[2]"
+    end
+end
